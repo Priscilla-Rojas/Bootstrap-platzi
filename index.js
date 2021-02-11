e469e4c9c44d954c63fe85eeea71068bccf6546f
Priscilla Rojas <priscilla.k.rojas@hotmail.com>
2021-02-11 09:40:56 -0300

    solucione el bug del menu desplegable

---
+++ index.js
@@ -0,0 +1,6 @@
+const menu = document.getElementById('menu');
+const navbar = document.getElementById('navbar');
+
+menu.addEventListener('click', (event)=>{
+    navbar.classList.toggle("collapse");
+});
